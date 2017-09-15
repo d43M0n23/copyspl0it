0902558ff1b5e7e549911310024589cef8e12f63
daemon-r7 <daemon@coremedia-solution.com>
2017-09-15 21:51:45 +0200

    update

---
+++ copyspl0it.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+######################################################################
+# copyspl0it.sh
+# search and copy Exploit from exploit-db 2 metasploit-framework
+# Needs searchsploit amd metasploit-framework installed
+# apt-get install mplayer lame
+# command: > copyspl0it.sh $searchsting1 $searchstring2 $searchstring3
+######################################################################
+
+######################################################################
+# last modify: 04.07.2017
+# bug or anything: d43M0n23@3xpl0it.com
+######################################################################
+COLOR:
+#######################################################################################
+
+# Bash sTyl3!
+clear='\033[0m'			#alle Attribute zurücksetzen
+red='\e[31m'
+bold='\033[1m'			#Fettschrift
+underline='\033[4m'		#Unterstreichen
+blinken='\033[5m'		#Blinken
+invers='\033[7m'		#inverse Darstellung
+black='\033[30m'		#Schriftfarbe schwarz
+red='\033[31m'			#Schriftfarbe rot
+green='\033[32m'		#Schriftfarbe grün
+yell='\033[33m'			#Schriftfarbe gelb
+blue='\033[34m'			#Schriftfarbe blau
+mag='\033[35m'			#Schriftfarbe magenta
+turk='\033[36m'			#Schriftfarbe türkis
+white='\033[37m'		#Schriftfarbe weiß
+########################################################################################
+
+
